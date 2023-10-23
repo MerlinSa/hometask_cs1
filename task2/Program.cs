89f28add3d7b9a6ea74355c712d7d058c7e2f484
Saikala <s.borubaevaa@gmail.com>
2023-10-23 14:13:42 +0600

    add task2

---
+++ task2/Program.cs
@@ -0,0 +1,22 @@
+﻿
+//Напишите прогу, кот-я ан вход принимает три числа,
+// и выдает максимальное из этих чисел.
+
+int a = 30;
+int b = 40;
+int c = 20;
+
+if (a > b && a > c) 
+{
+    Console.WriteLine(a + "=max");
+}
+else { 
+    if (b > a && b > c)
+        Console.WriteLine(b + "=max");
+
+    else {
+        Console.WriteLine(c + "=max");
+    }    
+
+}
+
