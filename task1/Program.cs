e9bcf5ad4b0deaa003364e18f4350c72a8425711
Saikala <s.borubaevaa@gmail.com>
2023-10-23 14:13:18 +0600

    add task1

---
+++ task1/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача1. Напишите прог-у, кот-я на вход принимает два числа и выдает,
+// какое число большее, ак какое меньшее.
+
+using System.Xml.XPath;
+
+int A = 25;
+int B = 10;
+
+if( A > B ) {
+    
+    Console.WriteLine(A + " большее число");
+}
+else {
+    Console.WriteLine (B + " большее число");
+}
+if (B < A)
+{
+    Console.WriteLine(B + " меньшее число");
+}
+else {
+    Console.WriteLine(A + " меньшее число");
+}
